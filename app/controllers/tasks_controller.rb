f1c2c5eec9ea75354fe1b602de6825292ea5550b
hamasaki-code <dazhibinqi@gmail.com>
2024-12-29 15:37:38 +0900

    Add task detail page with show action and navigation links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -38,6 +39,10 @@ class TasksController < ApplicationController
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :description, :due_date, :completed)
