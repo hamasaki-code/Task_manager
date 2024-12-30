2a40b46143f62ebafc2daa64265ba3972c1dab75
hamasaki-code <dazhibinqi@gmail.com>
2024-12-30 18:35:04 +0900

    Implement task deletion functionality with confirmation and user scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "home/index"
-  resources :tasks, only: [:new, :create, :index, :show, :edit, :update]
+  resources :tasks
   devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
